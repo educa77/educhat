43987717bbd3265cb2aa6de902422d55b46669b6
educalo77 <educalo@outlook.com>
2020-12-03 13:16:00 -0300

    upto modal create contact

---
+++ client/src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Login from './Login';
+import useLocalStorage from '../hooks/useLocalStorage';
+import Dashboard from './Dahboard';
+
+
+function App() {
+
+  const [id, setId] = useLocalStorage('id');
+
+  return (
+
+      id ? <Dashboard id={id} /> : <Login onIdSubmit={setId} />
+
+  );
+}
+
+export default App;
